619026561f4a11c7081f4f9dbb02df8fa892b687
aqilaaxd <khususgamepremium999@gmail.com>
2026-08-31 16:50:02 +0700

    Create Project.js

---
+++ models/Project.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+  zipFile: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Project', projectSchema);
